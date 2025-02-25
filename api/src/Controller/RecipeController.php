89247d7c21d2dd8d0175fd1398de041f13859fb6
Nawal69200 <nawal.kajtou@gmail.com>
2025-02-25 15:49:26 +0100

    initial commit

--- api/src/Controller/RecipeController.php
+++ api/src/Controller/RecipeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 final class RecipeController extends AbstractController
 {
     #[Route('/recipe', name: 'app_recipe')]
-    public function index(): Response
+    public function index(RecipeRepository $repository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER'); // si pas authentifier, pas acces a cette page
+
         return $this->render('recipe/index.html.twig', [
             'controller_name' => 'RecipeController',
         ]);
